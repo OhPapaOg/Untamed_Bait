80fb5ea7665c53ce3d165895358916d34202d399
OhPapa <154534922+OhPapaOg@users.noreply.github.com>
2024-07-24 15:01:10 -0700

    Fixed a vulnerability in the server side code. Involving retunrbait and returntrap. Credits go to Safser

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,9 @@
 local VorpCore = {}
 local VorpInv = {}
 
+local playerBaits = {}
+local bearTraps = {}
+
 TriggerEvent("getCore", function(core)
     VorpCore = core
 end)
@@ -20,6 +23,10 @@ Citizen.CreateThread(function()
     for itemName, _ in pairs(Config.BaitItems) do
         VorpInv.RegisterUsableItem(itemName, function(data)
             local _source = data.source
+
+
+            playerBaits[_source] = true 
+            if Config.Debug then print('Bait: ', playerBaits[_source]) end
             VorpInv.CloseInv(data.source)
             VorpInv.subItem(_source, itemName, 1)
             if Config.Debug then print('Usable item registered: ' .. itemName) end
@@ -29,6 +36,10 @@ Citizen.CreateThread(function()
     if Config.BearTrap.Enabled then
         VorpInv.RegisterUsableItem(Config.BearTrap.ItemName, function(data)
             local _source = data.source
+
+            bearTraps[_source] = true
+
+            if Config.Debug then print('Trap: ', playerBaits[_source]) end
             VorpInv.CloseInv(_source)
             VorpInv.subItem(_source, Config.BearTrap.ItemName, 1) -- Ensure item is removed on placement
             if Config.Debug then print('Bear trap usable item registered: ' .. Config.BearTrap.ItemName) end
@@ -40,13 +51,36 @@ end)
 RegisterServerEvent('untamed_bait:returnBearTrap')
 AddEventHandler('untamed_bait:returnBearTrap', function()
     local _source = source
+
+    if not bearTraps[_source] then
+        return
+    end
+
+    if Config.Debug then print('Beartrap1: ', bearTraps[_source]) end
     VorpInv.addItem(_source, Config.BearTrap.ItemName, 1) -- Return the bear trap item
     VorpCore.NotifyRightTip(_source, Config.Locale.returnBait, 4000)
+    bearTraps[source] = nil
+    if Config.Debug then print('Beartrap2: ', bearTraps[_source]) end
 end)
 
 RegisterServerEvent('untamed_bait:returnBait')
 AddEventHandler('untamed_bait:returnBait', function(itemName)
+
     local _source = source
-    VorpInv.addItem(_source, itemName, 1)
-    VorpCore.NotifyRightTip(_source, Config.Locale.returnBait, 4000)
+    if not playerBaits[_source] then
+        return
+    end
+
+    if Config.Debug then print('Baits1: ', playerBaits[_source]) end
+
+    for baitName, _ in pairs(Config.BaitItems) do
+        if baitName == itemName then
+            VorpInv.addItem(_source, itemName, 1)
+            VorpCore.NotifyRightTip(_source, Config.Locale.returnBait, 4000)
+            playerBaits[_source] = nil
+        end
+    end
+
+    if Config.Debug then print('Baits2: ', playerBaits[_source]) end
+
 end)
